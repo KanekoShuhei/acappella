71923c8e4cca64679052afd5c14ac6275e312e54
Shuhei Kaneko <shuhei.kaneko@rakus.co.jp>
2017-10-27 09:57:27 +0900

    add_promise

--- src/main/webapp/js/video_list.js
+++ src/main/webapp/js/video_list.js
@@ -1,29 +1,28 @@
 var myapp = angular.module("myapp", []);
-myapp.controller('mainCtrl', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
-	
-	$http({
-	    method: 'GET',
-	    url: '/ac/video/get-all-json'
-	}).then(function(videos) {
-		showVideos(videos);
-	});
-	
-	function showVideos(videos) {
+myapp.controller('mainCtrl', [
+  '$scope',
+  '$http',
+  '$sce',
+  function($scope, $http, $sce) {
 
-        $scope.results = [];
+    $http({method: 'GET', url: '/ac/video/get-all-json'}).then(function(videos) {
+      showVideos(videos);
+    });
 
-        angular.forEach(videos.data, function(value, index, array) {
+    function showVideos(videos) {
 
-        	var like = $sce.trustAsHtml(
-                    '<a href="' + ctx + '/video/addlikes?videoId=' + value.id + '"' +
-        			' class="btn btn-default btn-xs" role="button">★</a>');
-            var hasLiked = false;
-            
-            $http({
-              method: 'GET',
-              url: '/ac/video/get-one-json-by-login-user'
-            }).success(function(loginUserLikeVideos) {
-            	
+      $scope.results = [];
+
+      angular.forEach(videos.data, function(value, index, array) {
+
+        var like = $sce.trustAsHtml('<a href="' + ctx + '/video/addlikes?videoId=' + value.id + '"' + ' class="btn btn-default btn-xs" role="button">★</a>');
+        var hasLiked = false;
+
+        var checkLiked = function() {
+          return new Promise(function(resolve, reject) {
+
+            $http({method: 'GET', url: '/ac/video/get-one-json-by-login-user'}).then(function(loginUserLikeVideos) {
+
               var arr = loginUserLikeVideos.data;
               arr.filter(function(item, index) {
                 if (item.id == value.id) {
@@ -31,32 +30,42 @@ myapp.controller('mainCtrl', ['$scope', '$http', '$sce', function($scope, $http,
                 }
               });
               if (hasLiked) {
-            	  like = $sce.trustAsHtml(
-                  '<a href="' + ctx + '/video/delete-like-videos?videoId=' + value.id + '"' +
-                  ' class="btn btn-default btn-xs" role="button" style="color:yellow;">★</a>');
+                like = $sce.trustAsHtml('<a href="' + ctx + '/video/delete-like-videos?videoId=' + value.id + '"' + ' class="btn btn-default btn-xs" role="button" style="color:yellow;">★</a>');
               }
-            });
-        	
-            var youtubeUrl = 'https://www.googleapis.com/youtube/v3/videos?' + ['id=' +
-                encodeURIComponent(value.youtubeId),
-                'key=AIzaSyCi73Clf3nXaxoocsBAp3d9ewlgGGjXuW4',
-                'part=snippet,statistics',
-            ].join('&');
+              
+              resolve();
+              
+            },function errorCallback(response) {
+            	console.log("ログインしていません");
+            	resolve();
+              });
 
-            $http({
-                method: 'GET',
-                url: youtubeUrl
-            }).then(function(data) {
-                $scope.trustSrc = function(src) {
-                    return $sce.trustAsResourceUrl('https://www.youtube.com/embed/' + src);
-                }
-                $scope.results.push({
-                    video: value, //動画情報
-                    data: data.data.items[0],//YouTube情報
-                    likeVideoButton: like //いいねボタン
-                    });
+          });
+        };
+
+        checkLiked().then(function() {
+
+          var youtubeUrl = 'https://www.googleapis.com/youtube/v3/videos?' + [
+            'id=' + encodeURIComponent(value.youtubeId),
+            'key=AIzaSyCi73Clf3nXaxoocsBAp3d9ewlgGGjXuW4',
+            'part=snippet,statistics'
+          ].join('&');
+
+          $http({method: 'GET', url: youtubeUrl}).then(function(data) {
+            $scope.trustSrc = function(src) {
+              return $sce.trustAsResourceUrl('https://www.youtube.com/embed/' + src);
+            }
+            $scope.results.push({
+              video: value, //動画情報
+              data: data.data.items[0], //YouTube情報
+              likeVideoButton: like //いいねボタン
             });
+          });
+
         });
-        console.dir($scope.results);
-	}
-}]);
+
+      });
+      console.dir($scope.results);
+    }
+  }
+]);
